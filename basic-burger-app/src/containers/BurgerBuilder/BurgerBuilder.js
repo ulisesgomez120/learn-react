2e6e0aeb3ce6f0dae143fb6f713ffde1532c6320
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-08-04 11:01:07 -0700

    added basic file structures

---
+++ basic-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Wrapper";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>burger</div>
+        <div>build controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
