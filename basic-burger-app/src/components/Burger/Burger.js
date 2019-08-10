7eedd50d33d2083b270bd31fa9d81b6a316d643a
Ulises Gomez <ulisesgomez120@gmail.com>
2019-08-09 20:18:05 -0700

    addedfunc for dynamically adding ingredients

--- basic-burger-app/src/components/Burger/Burger.js
+++ basic-burger-app/src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import classes from "./Burger.module.css";
 import Ingredients from "./Ingredients/Ingredients";
 
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(iKey => {
+    return [...Array(props.ingredients[iKey])].map((_, i) => {
+      return <Ingredients key={iKey + 1} type={iKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <Ingredients type="bread-top" />
-      <Ingredients type="cheese" />
-      <Ingredients type="meat" />
+      {transformedIngredients}
       <Ingredients type="bread-bottom" />
     </div>
   );
