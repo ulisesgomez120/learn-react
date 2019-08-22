92445849c57f26c6739750afc98cb67d0e225716
Ulises Gomez <ulisesgomez120@gmail.com>
2019-08-21 20:27:08 -0700

    added functionality to handle no ingredients

--- basic-burger-app/src/components/Burger/Burger.js
+++ basic-burger-app/src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from "./Burger.module.css";
 import Ingredients from "./Ingredients/Ingredients";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(iKey => {
-    return [...Array(props.ingredients[iKey])].map((_, i) => {
-      return <Ingredients key={iKey + 1} type={iKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(iKey => {
+      return [...Array(props.ingredients[iKey])].map((_, i) => {
+        return <Ingredients key={iKey + 1} type={iKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add some ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <Ingredients type="bread-top" />
